39766f98b8f432e2aec1d85956bcdd40b695e411
Mjeppo <mjeppo@live.nl>
2026-03-05 14:29:29 +0100

    Andere ddns

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,6 +1,6 @@
-import PocketBase from 'pocketbase';
+import PocketBase from 'pocketbase'
 
 // We verbinden direct met jouw nieuwe server!
-const pb = new PocketBase('https://mjeppo.ddns.net');
+const pb = new PocketBase('https://mjeppo.duckdns.net')
 
-export default pb;
+export default pb
