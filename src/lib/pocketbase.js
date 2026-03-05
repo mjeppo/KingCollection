1c3324fb94a10d6d1a9f1ab32116171765322192
Mjeppo <mjeppo@live.nl>
2026-03-05 14:33:33 +0100

    Correctie dns adres

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase'
 
 // We verbinden direct met jouw nieuwe server!
-const pb = new PocketBase('https://mjeppo.duckdns.net')
+const pb = new PocketBase('https://mjeppo.duckdns.org')
 
 export default pb
